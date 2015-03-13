637e0a67e3c6ddffe0ffcc18e2999f92301514bf
Zepeng Li <zepeng.li@gmail.com>
2015-03-13 15:34:16 +0800

    Create Schema Action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,8 +2,10 @@ package controllers
 
 import _root_.helpers.AppConfig
 import controllers.session._
+import models.Schemas
 import models.cfs.Path
 import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 import views._
 
@@ -23,4 +25,9 @@ object Application extends Controller with AppConfig {
     val videoPath = config.getString("wiki.video").map(Path(_))
     Ok(html.static_pages.wiki(videoPath))
   }
+
+  def recreate = UserAction.async {
+    Schemas.create.map {_ => Ok("Schema Created")}
+  }
+
 }
---
+++ app/models/Schemas.scala
@@ -0,0 +1,27 @@
+package models
+
+import com.datastax.driver.core.ResultSet
+import models.cassandra.Cassandra
+import models.cfs._
+import models.sys.SysConfig
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.concurrent.Future
+import scala.language.postfixOps
+
+/**
+ * @author zepeng.li@gmail.com
+ */
+object Schemas extends Cassandra {
+
+  def create: Future[Seq[ResultSet]] = Future.sequence(
+    Seq(
+      SysConfig.create.future(),
+      User.create.future(),
+      UserByEmail.create.future(),
+      INode.create.future(),
+      IndirectBlock.create.future(),
+      Block.create.future()
+    )
+  )
+}
