b9d1f7e264acf1eb8adb4ecbd16a120b1659df57
Zepeng Li <zepeng.li@gmail.com>
2015-04-25 12:25:40 +0800

    Trying to introduce typeahead form control

--- modules/api/app/controllers/api/Users.scala
+++ modules/api/app/controllers/api/Users.scala
@@ -33,8 +33,8 @@ object Users
       }
     }
 
-  def search(keyword: String) =
+  def search(q: String) =
     PermCheck(_.Show).async { implicit req =>
-      ES.Search(User)(keyword).map {Ok(_)}
+      ES.Search(User)(q).map {Ok(_)}
     }
 }
