7b7f70be92022100b2ab86c8181dbe1bb14e217c
Zepeng Li <zepeng.li@gmail.com>
2016-03-24 14:12:42 +0800

    Log error occurred in JsonStringifier

--- modules/models/app/helpers/JsonStringifier.scala
+++ modules/models/app/helpers/JsonStringifier.scala
@@ -14,9 +14,14 @@ trait JsonStringifier[A] extends Logging {
   def default: A
 
   def fromJson(obj: String): A = Try(Json.parse(obj)) match {
-    case Success(json) => jsonFormat.reads(json).getOrElse(default)
+    case Success(json) => jsonFormat.reads(json) match {
+      case JsSuccess(value, _) => value
+      case e: JsError          =>
+        Logger.warn(s"Json parse succeeded but read failed. Reverts to default value: ${e.toString}")
+        default
+    }
     case Failure(e)    =>
-      Logger.warn("Json parse failed and reverts to default value", e)
+      Logger.warn("Json parse failed. Reverts to default value", e)
       default
   }
 
