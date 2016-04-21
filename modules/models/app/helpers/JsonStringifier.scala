becbff1bf9c5965b8977cbe8ef1d483909c4143d
Zepeng Li <zepeng.li@gmail.com>
2016-04-21 21:31:49 +0800

    Improved JsonStringifier trait

--- modules/models/app/helpers/JsonStringifier.scala
+++ modules/models/app/helpers/JsonStringifier.scala
@@ -7,23 +7,35 @@ import scala.util._
 /**
  * @author zepeng.li@gmail.com
  */
-trait JsonStringifier[A] extends Logging {
-
-  def jsonFormat: Format[A]
+trait JsonStringifier[A] extends JsonOptionalStringifier[A] {
 
   def default: A
 
-  def fromJson(obj: String): A = Try(Json.parse(obj)) match {
+  def fromJson(obj: String): A = optionalFromJson(obj).getOrElse(default)
+}
+
+trait JsonOptionalStringifier[A] extends Logging {
+
+  def jsonFormat: Format[A]
+
+  def optionalFromJson(obj: String): Option[A] = Try(Json.parse(obj)) match {
     case Success(json) => jsonFormat.reads(json) match {
-      case JsSuccess(value, _) => value
+      case JsSuccess(value, _) => Some(value)
       case e: JsError          =>
         Logger.warn(s"Json parse succeeded but read failed. Reverts to default value: ${e.toString}")
-        default
+        None
     }
     case Failure(e)    =>
       Logger.warn("Json parse failed. Reverts to default value", e)
-      default
+      None
   }
 
   def toJson(obj: A): String = Json.stringify(jsonFormat.writes(obj))
+}
+
+object JsonOptionalStringifier {
+
+  implicit def jsonStringifier[A](implicit fmt: Format[A]) = {
+    new JsonOptionalStringifier[A] {def jsonFormat = fmt}
+  }
 }
