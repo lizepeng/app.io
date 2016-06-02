a559bbe8fcfd92b49e27a122dd085cdf904ea1af
Zepeng Li <zepeng.li@gmail.com>
2016-06-02 12:20:52 +0800

    Added convenient methods for Json formatting

--- modules/models/app/helpers/ExtReads.scala
+++ modules/models/app/helpers/ExtReads.scala
@@ -11,6 +11,30 @@ object ExtReads {
     def reads(json: JsValue) = JsSuccess(default)
   }
 
+  implicit class RichJsPath(val path: JsPath) extends AnyVal {
+
+    def formatNullable1[T](
+      implicit f: Format[T]
+    ): OFormat[Option[T]] = OFormat(
+      Reads.nullable(path)(f),
+      Writes.at(path)(Writes.optionWithNull[T](f))
+    )
+
+    def formatNullable1[T](r: Reads[T])(
+      implicit w: Writes[T]
+    ): OFormat[Option[T]] = OFormat(
+      Reads.nullable(path)(r),
+      Writes.at(path)(Writes.optionWithNull[T](w))
+    )
+
+    def formatNullable1[T](w: Writes[T])(
+      implicit r: Reads[T]
+    ): OFormat[Option[T]] = OFormat(
+      Reads.nullable(path)(r),
+      Writes.at(path)(Writes.optionWithNull[T](w))
+    )
+  }
+
   implicit def optionalFormat[T](implicit jsFmt: Format[T]): Format[Option[T]] =
     new Format[Option[T]] {
       override def reads(json: JsValue): JsResult[Option[T]] = json match {
